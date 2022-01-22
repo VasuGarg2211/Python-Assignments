9c9f0b9c21f6697f8ae56820f21e3991d78a168c
Vasuman <97223228+VasumanGarg@users.noreply.github.com>
2022-01-23 00:48:52 +0530

    Add files via upload

---
+++ Asg_2_ECE_21105083_OUTPUT.py
@@ -0,0 +1,42 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\vasum\Desktop\VS code\Python_Asgs\Assignment 2\Asg_2_ECE_21105083.py
+       Question 1
+1.a--> Length of the input string is: 35
+1.b--> Reverse of input string is: egaugnal evitisnes esac a si nohtyP
+1.c--> Sliced string is: a case sensitive
+1.d--> String after replacing is: Python is object oriented language
+1.e--> Index of substring "a" is: 10
+1.f--> String after removing " " is: Pythonisacasesensitivelanguage
+
+
+         Question 2
+Hey, Vasuman Here!
+My SID is 21105083
+I am from ECE department and my CGPA is 9.500000
+
+
+         Question 3
+3.a--> a&b= 8
+3.b--> a|b= 58
+3.c--> a^b= 50
+3.d--> a<<2= 224 and b<<2= 40
+3.e--> a>>2= 14 and b>>4= 0
+         Question 4
+Enter first number:25
+Enter second number:40
+Enter third number:15
+greatest of three numbers is 40
+
+
+         Question 5
+Enter a string:my name is vasu
+Yes
+
+
+         Question 6
+Enter first length:3
+Enter second length:4
+Enter third length:5
+Yes
