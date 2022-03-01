a1469a8dc3c482ba11a0ccda384987f44e90e9dd
Vasuman <97223228+VasumanGarg@users.noreply.github.com>
2022-03-01 16:05:30 +0530

    Add files via upload

---
+++ Asg4_ECE_21105083_OUTPUT.py
@@ -0,0 +1,101 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:\Users\vasum\Desktop\VS code\Python_Asgs\Assignment 4\Asg4_ECE_21105083.py
+*** Question 1 ***
+
+Enter number of disk in tower of Hanoi:3
+
+The Disks are numbered starting from top of the tower.
+Steps to move all disks in order are given below:
+
+Move disk 1 from Source to Destination
+Move disk 2 from Source to Auxiliary
+Move disk 1 from Destination to Auxiliary
+Move disk 3 from Source to Destination
+Move disk 1 from Auxiliary to Source
+Move disk 2 from Auxiliary to Destination
+Move disk 1 from Source to Destination
+
+*** Question 2 ***
+
+The Pascal's Triangle using recursive approach:
+
+Enter number of rows in Pascle's Triangle:5
+                                                 1                                                  
+                                                1  1                                                
+                                              1  2  1                                               
+                                             1  3  3  1                                             
+                                           1  4  6  4  1                                            
+
+The Pascal's Triangle using Iterative approach is:
+
+Enter number of rows:6
+                                                 1                                                  
+                                                1  1                                                
+                                              1  2  1                                               
+                                             1  3  3  1                                             
+                                           1  4  6  4  1                                            
+                                         1  5  10  10  5  1                                         
+
+*** Question 3 ***
+
+
+Enter an Integer:50
+Enter another Integer:3
+
+The quotient when 50 is divided by 3 is 16.
+The remainder when 50 is divided by 3 is 2.
+
+Question 3.a
+Function is not-callable
+
+Question 3.b
+All result values are non-zero
+
+Question 3.c
+Appended (4,5,6) in the values (16,2)
+Values greater than 4 is 16,5,6
+
+Question 3.d
+Above result in set form is shown below:
+{16, 5, 6}
+
+Question 3.e
+The above set is now immutable.
+
+Question 3.f
+Maximum value from set is 16
+Hash value of 16 is 16
+
+*** Question 4 ***
+
+Hello, My name is Vasu and my SID is 21105083
+Destructor Called
+
+*** Question 5 ***
+
+employee name is Mehak and salary is 40000 
+employee name is Ashok and salary is 50000 
+employee name is Viren and salary is 60000 
+
+Question 5.a
+
+Mehak salary Updated to 70000
+employee name is Mehak and salary is 70000 
+
+Question 5.b
+Employee Viren's data has been removed.
+
+*** Question 6 ***
+
+
+					WELCOME TO THE FRIENDSHIP GAME
+
+ enter name of player 1:vasu
+enter name of player 2:vansh
+
+enter word by vasu:karan
+enter word by vansh:narak
+
+Friendship of vasu and vansh is REAL
